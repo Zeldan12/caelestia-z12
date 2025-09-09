47d5392c8ac956453e7aebbb5aca6ec9e565d0d1
Zeldan12 <amado.s.isac@gmail.com>
2025-09-09 21:27:19 +0100

    Zeldan configurations

--- fish/config.fish
+++ fish/config.fish
@@ -1,9 +1,28 @@
+zoxide init fish | source
+fnm env --use-on-cd | source
+
+set -gx EDITOR nvim
+set -gx PNPM_HOME /home/isac/.local/share/pnpm
+
+if not contains $PNPM_HOME $PATH
+    set -gx PATH $PNPM_HOME $PATH
+end
+
+function y
+    set tmp (mktemp -t "yazi-cwd.XXXXXX")
+    yazi $argv --cwd-file="$tmp"
+    if read -z cwd <"$tmp"; and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+        builtin cd -- "$cwd"
+    end
+    rm -f -- "$tmp"
+end
+
 if status is-interactive
     # Starship custom prompt
     starship init fish | source
 
     # Custom colours
-    cat ~/.local/state/caelestia/sequences.txt 2> /dev/null
+    cat ~/.local/state/caelestia/sequences.txt 2>/dev/null
 
     # For jumping between prompts in foot terminal
     function mark_prompt_start --on-event fish_prompt
--- fish/functions/fish_greeting.fish
+++ fish/functions/fish_greeting.fish
@@ -1,10 +1,11 @@
 function fish_greeting
-    echo -ne '\x1b[38;5;16m'  # Set colour to primary
-    echo '     ______           __          __  _       '
-    echo '    / ____/___ ____  / /__  _____/ /_(_)___ _ '
-    echo '   / /   / __ `/ _ \/ / _ \/ ___/ __/ / __ `/ '
-    echo '  / /___/ /_/ /  __/ /  __(__  ) /_/ / /_/ /  '
-    echo '  \____/\__,_/\___/_/\___/____/\__/_/\__,_/   '
+    echo -ne '\x1b[38;5;16m' # Set colour to primary
+    echo '  _____     _     _             '
+    echo ' / _  / ___| | __| | __ _ _ __  '
+    echo ' \\// / / _ \ |/ _` |/ _` | \'_ \ '
+    echo '  / //\\  __/ | (_| | (_| | | | |'
+    echo ' /____/\\___|_|\__,_|\__,_|_| |_|'
+    echo '                        Caelestia'
     set_color normal
     fastfetch --key-padding-left 5
 end
